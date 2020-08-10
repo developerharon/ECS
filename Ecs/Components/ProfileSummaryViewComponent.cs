35944dc8e05c6c7d74a1311ec767b2810574abef
Haron Kipkorir <developerharon@users.noreply.github.com>
2020-08-10 23:07:46 +0300

    updated the ui

---
+++ Ecs/Components/ProfileSummaryViewComponent.cs
@@ -0,0 +1,23 @@
+﻿using Ecs.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace Ecs.Components
+{
+    public class ProfileSummaryViewComponent : ViewComponent
+    {
+        private UserManager<ApplicationUser> _userManager;
+
+        public ProfileSummaryViewComponent(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var user = _userManager.Users.Where(user => user.UserName == User.Identity.Name).FirstOrDefault();
+            return View(user);
+        }
+    }
+}
