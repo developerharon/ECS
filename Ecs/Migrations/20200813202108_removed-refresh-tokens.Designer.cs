717771c0413cb08d1d134cb81611255f3b87559f
Haron Kipkorir <developerharon@users.noreply.github.com>
2020-08-13 23:25:13 +0300

    Updated database migrations

--- Ecs/Migrations/20200813202108_removed-refresh-tokens.Designer.cs
+++ Ecs/Migrations/20200813202108_removed-refresh-tokens.Designer.cs
@@ -4,14 +4,16 @@ using Ecs.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Ecs.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200813202108_removed-refresh-tokens")]
+    partial class removedrefreshtokens
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
